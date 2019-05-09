e3e55c2a6bf854707c899f473dfd7908e815cf49
Isaac de Souza <isaacde2012@gmail.com>
2019-05-09 20:31:05 -0300

    classe principal criada, começando agora o maçante processo de documentar cada coisinha

---
+++ index.js
@@ -0,0 +1,11 @@
+//class creation
+function Estabelecimento(nome,orcamento,percentualDeCorte,textoIntrodutorio){
+    this.nome = nome;
+    this.orcamento = orcamento;
+    this.percentualDeCorte = percentualDeCorte;
+    this.textoIntrodutorio = textoIntrodutorio;
+};
+
+//------------------INSTANCIAS--------------------//
+
+//-------Amapá---------//
