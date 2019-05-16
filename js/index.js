aead1c5981f1aa3e1a9cde679f3893c0ba096715
Isaac de Souza <isaacde2012@gmail.com>
2019-05-16 16:53:34 -0300

    Tabela quase pronta, no momento estou documentando as informações dos estados em suas respectivas variaveis para que possa fazer a função de consulta na tabela,provavelmente seria bem mais organizado com o BD, mas a gente n aprendeu isso ainda.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,42 @@
 //class creation
-function Estabelecimento(nome,orcamento,percentualDeCorte,textoIntrodutorio){
+function Estabelecimento(nome,orcamento,percentualDeCorte,valorCorteBruto){
     this.nome = nome;
     this.orcamento = orcamento;
     this.percentualDeCorte = percentualDeCorte;
-    this.textoIntrodutorio = textoIntrodutorio;
+    this.valorCorteBruto = valorCorteBruto;
 };
 
 
 //estabelecendo variaveis
 
-//lista os estados
+//isso só existe pq ficar digitando todos eles é um saco, pqp
+let stringEstados = ["Acre","Alagoas","Amapá","Amazonas","Bahia",'Ceara','Distrito Federal','Espírito Santo',
+'Goiás','Maranhão','Mato Grosso',"Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná",
+"Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia",
+"Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"];
+
+//seleciona os estados no site
 let vetorEstados = $(".estado");
 
 // estadoSelecionado configura o que vai aparecer na página de acordo com o estado. mais informações na documentação do github
 let estadoSelecionado;
+let estadoMouseoverString;
+
+//verificando o estado do menu
+let menuDeBotoes = false;
+
+
+//-------------------------BOTÕES--------------------//
+
+
+let listaBotoes = $(".options button");
+let estabelecimentoSelecionado;
+for (let index = 0; index < listaBotoes.length; index++) {
+        listaBotoes[index].addEventListener("click",()=>{
+            estabelecimentoSelecionado = listaBotoes[index].getAttribute("id");
+        });  
+};
+
 
 
 
@@ -27,16 +50,28 @@ let estadoSelecionado;
 for (let index = 0; index < vetorEstados.length; index++){
     vetorEstados[index].addEventListener("click",function (){
         console.log("click funcionando bro");
-        $(".estados").fadeOut();
+        estadoSelecionado =  vetorEstados[index].getAttribute("id");
+        menuDeBotoes = true;
+        showButtons(menuDeBotoes);
+    });
+    vetorEstados[index].addEventListener("mouseover",function(event){
+        estadoMouseoverString = ' <img src="./assets/images/' + vetorEstados[index].getAttribute("id") +'.png"> ';
+        vetorEstados[index].innerHTML  = estadoMouseoverString + stringEstados[index];
+
     });
 };
 
 
 //------------------INSTANCIAS--------------------//
 
+//----------Acre-----------//
+let UF_Acre = new Estabelecimento("Universidade Federal do Acre","58.672.830","7%","3.834.634");
+let IF_Acre = new Estabelecimento("Instituto Federal do Acre");
+
 //-------Amapá---------//
 
 let UF_Amapa = [];
+let IF_Amapa = [];
 
 
 
@@ -45,6 +80,18 @@ let UF_Amapa = [];
 
 //-------------------FUNÇÕES GERAIS----------------//
 
-function showButtons(){
-
+function showButtons(menu){
+    if(menu){
+        $(".estados").fadeOut();    
+        $(".options").fadeIn();
+    }
+    else{
+        $(".options").fadeOut();
+        $(".estados").fadeIn();
+    }
 };
+
+
+function showInfo(estabelecimento,estado){
+
+}
