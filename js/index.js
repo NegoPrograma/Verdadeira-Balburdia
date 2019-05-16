772193da4ab122cc2781c7d5eed8345431b67aef
Isaac de Souza <isaacde2012@gmail.com>
2019-05-16 14:47:13 -0300

    por alguma razão a função de click do jquery n ta funcionando, vou checar isso direitinho mais tarde. mas o resto das funções esta, fadeOut configurado com sucesso.

--- js/index.js
+++ js/index.js
@@ -9,15 +9,42 @@ function Estabelecimento(nome,orcamento,percentualDeCorte,textoIntrodutorio){
 
 //estabelecendo variaveis
 
-estado = $("estado");
+//lista os estados
+let vetorEstados = $(".estado");
+
+// estadoSelecionado configura o que vai aparecer na página de acordo com o estado. mais informações na documentação do github
+let estadoSelecionado;
+
+
+
+
 
 
 //-------------TABELAS----------------//
 
+//eventos das tabelas
+
+for (let index = 0; index < vetorEstados.length; index++){
+    vetorEstados[index].addEventListener("click",function (){
+        console.log("click funcionando bro");
+        $(".estados").fadeOut();
+    });
+};
 
 
 //------------------INSTANCIAS--------------------//
 
 //-------Amapá---------//
 
-let UF_Amapa = []
+let UF_Amapa = [];
+
+
+
+
+
+
+//-------------------FUNÇÕES GERAIS----------------//
+
+function showButtons(){
+
+};
